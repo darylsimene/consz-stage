4810dd59780ab7340d60258927c6bfd0912d360b
John Rey <john.cagaanan@supportzebra.com>
2023-05-09 01:41:58 +0800

    Testing component refactored

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/ccp" element={<CCP />} />
-                <Route path="/ccp" element={<Testing />} />
+                <Route path="/testing" element={<Testing />} />
 
             </Routes>
         </div>
--- src/main/components/Testing.js
+++ src/main/components/Testing.js
@@ -1,18 +1,50 @@
+// const AWS = require('aws-sdk');
+// const connect = new AWS.Connect();
+
+// const listUsers = async () => {
+//   const params = {
+//     InstanceId: '408781860680',
+//     region: 'us-east-1',
+//   };
+  
+//   try {
+//     const response = await connect.listUsers(params).promise();
+//     console.log(response.Users);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// listUsers();
+import React, { useEffect } from 'react'
 const AWS = require('aws-sdk');
 const connect = new AWS.Connect();
 
-const listUsers = async () => {
-  const params = {
-    InstanceId: '408781860680',
-    region: 'us-east-1',
+
+
+const Testing = () => {
+
+  const listUsers = async () => {
+    const params = {
+      InstanceId: '408781860680',
+      // region: 'us-east-1',
+    };
+    
+    try {
+      const response = await connect.listUsers(params).promise();
+      console.log(response.Users);
+    } catch (error) {
+      console.error(error);
+    }
   };
   
-  try {
-    const response = await connect.listUsers(params).promise();
-    console.log(response.Users);
-  } catch (error) {
-    console.error(error);
-  }
-};
+  useEffect(()=>{
+    listUsers();
+  }, [])
 
-listUsers();
+  return (
+    <div>Testing</div>
+  )
+}
+
+export default Testing
