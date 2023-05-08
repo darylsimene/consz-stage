2673979a40b4006864f9bebdc85f07dacdbb5ec3
John Daryl Simene <john.simene@supportzebra.com>
2023-05-09 04:47:14 +0800

    UI dashboard for all status

--- src/main/components/ActivityContainer.js
+++ src/main/components/ActivityContainer.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { BsPersonCircle } from "react-icons/bs";
+import { MdDialpad } from "react-icons/md";
 
 import StatusDropdown from "../mini-components/StatusDropdown";
 import "../css/ActivityContainer.css";
@@ -22,11 +23,36 @@ const ActivityContainer = () => {
                 </div>
             </div>
             <div className="conversation-box h100per ">
-                <div className="on-break-box h100per ">
-                    <div className="text-box">
-                        <img src="offline.png" alt="You-are-offline" className="img-w300px" />
-                        <h5>You are offline</h5>
-                    </div>
+                <div className="status-box-text h100per ">
+                    {userStatus === "Offline" && (
+                        <div className="text-box">
+                            <img src="offline.png" alt="You-are-offline" className="img-w300px" />
+                            <h5>You're currently unavailable</h5>
+                            <p>Set your status to available to start accepting new contacts</p>
+                        </div>
+                    )}
+                    {userStatus === "Online" && (
+                        <div className="text-box">
+                            <img src="online.png" alt="You-are-online" className="img-w300px" />
+                            <h5>You can start a call now.</h5>
+                            <button className="outboundcall-btn">
+                                <MdDialpad />
+                                Outbound Call
+                            </button>
+                        </div>
+                    )}
+                    {userStatus === "Break" && (
+                        <div className="text-box">
+                            <img src="break.png" alt="You-are-on-break" className="img-w300px" />
+                            <h5>You are on break.</h5>
+                        </div>
+                    )}
+                    {userStatus === "Lunch Break" && (
+                        <div className="text-box">
+                            <img src="lunch-break.png" alt="You-are-online" className="img-w300px" />
+                            <h5>Enjoy your lunch!</h5>
+                        </div>
+                    )}
                 </div>
             </div>
             {/* <div className="logs-container">
--- src/main/components/AwsConnectLogin.js
+++ src/main/components/AwsConnectLogin.js
@@ -1,49 +1,49 @@
-import React from "react";
-import AWS from "aws-sdk";
+// import React from "react";
+// import AWS from "aws-sdk";
 
-const connect = new AWS.Connect({
-    endpoint: "arn:aws:connect:us-east-1:408781860680:instance/9e4dafb4-fe24-497a-83d8-470ce575ade5",
-    region: "us-east-1",
-});
+// const connect = new AWS.Connect({
+//     endpoint: "arn:aws:connect:us-east-1:408781860680:instance/9e4dafb4-fe24-497a-83d8-470ce575ade5",
+//     region: "us-east-1",
+// });
 
-connect.listInstances({}, function (err, data) {
-    if (err) {
-        console.log("Not logged in to Amazon Connect");
-        // Redirect the user to the Amazon Connect login page
-        // window.location.href = "https://consz.awsapps.com/";
-        console.log(err);
-    } else {
-        console.log("Logged in to Amazon Connect");
-        // Do something with the logged in user, such as call an API
-    }
-});
+// connect.listInstances({}, function (err, data) {
+//     if (err) {
+//         console.log("Not logged in to Amazon Connect");
+//         // Redirect the user to the Amazon Connect login page
+//         // window.location.href = "https://consz.awsapps.com/";
+//         console.log(err);
+//     } else {
+//         console.log("Logged in to Amazon Connect");
+//         // Do something with the logged in user, such as call an API
+//     }
+// });
 
-const connect = require("amazon-connect-streams");
+// const connect = require("amazon-connect-streams");
 
-// Check if an agent is logged in
-if (connect.core.getAgent().agentId) {
-    console.log("An agent is currently logged in.");
-} else {
-    console.log("No agent is currently logged in.");
-}
+// // Check if an agent is logged in
+// if (connect.core.getAgent().agentId) {
+//     console.log("An agent is currently logged in.");
+// } else {
+//     console.log("No agent is currently logged in.");
+// }
 
-const connect = require("amazon-connect-streams");
+// const connect = require("amazon-connect-streams");
 
-// Initialize the connect.core object
-connect.core.initCCP({ ccpUrl: "https://consz.my.connect.aws/connect/ccp" });
+// // Initialize the connect.core object
+// connect.core.initCCP({ ccpUrl: "https://consz.my.connect.aws/connect/ccp" });
 
-// Wait for the CCP to initialize
-connect.core.whenConnected(function () {
-    // Check if an agent is logged in
-    if (connect.core.getAgent().agentId) {
-        console.log("An agent is currently logged in.");
-    } else {
-        console.log("No agent is currently logged in.");
-    }
-});
+// // Wait for the CCP to initialize
+// connect.core.whenConnected(function () {
+//     // Check if an agent is logged in
+//     if (connect.core.getAgent().agentId) {
+//         console.log("An agent is currently logged in.");
+//     } else {
+//         console.log("No agent is currently logged in.");
+//     }
+// });
 
-const AwsConnectLogin = () => {
-    return <div>AwsConnectLogin</div>;
-};
+// const AwsConnectLogin = () => {
+//     return <div>AwsConnectLogin</div>;
+// };
 
-export default AwsConnectLogin;
+// export default AwsConnectLogin;
